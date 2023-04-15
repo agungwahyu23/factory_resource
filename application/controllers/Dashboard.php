7859a0c8d026e0903730f3baa5764056b0ad4bc2
agungwahyu23 <52806335+agungwahyu23@users.noreply.github.com>
2023-04-15 16:39:46 +0700

    dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -12,6 +12,7 @@ class Dashboard extends CI_Controller {
 
 	public function loadkonten($page, $data) {
 		$data['username'] 	= $this->session->userdata('username');
+		$data['name_of_employee'] 	= $this->session->userdata('name_of_employee');
 
 		$this->load->view($page, $data);
 	}
@@ -20,6 +21,20 @@ class Dashboard extends CI_Controller {
 	{
 		$data['page'] 		= "Dashboard";
 		$data['content'] 	= "admin/dashboard";
+
+		$data['item'] 		= $this->db->query("SELECT COUNT(id) as item FROM tb_item")->row();
+
+		$data['material'] 		= $this->db->query("SELECT COUNT(id) as material FROM tb_raw_material")->row();
+
+		$data['req_sub'] 		= $this->db->query("SELECT COUNT(id) as result FROM tb_order WHERE status = 1")->row();
+		$data['req_acc'] 		= $this->db->query("SELECT COUNT(id) as result FROM tb_order WHERE status = 2")->row();
+		$data['req_rej'] 		= $this->db->query("SELECT COUNT(id) as result FROM tb_order WHERE status = 3")->row();
+
+		$data['return'] 		= $this->db->query("SELECT COUNT(id) as result FROM tb_return")->row();
+
+		$data['roadmap'] 		= $this->db->query("SELECT COUNT(id) as result FROM tb_roadmap")->row();
+
+		$data['employee'] 		= $this->db->query("SELECT COUNT(id) as result FROM tb_employee")->row();
 
 		$this->loadkonten('admin/app_base',$data);
 	}
--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,3 +1,9 @@
+<div class="col-xl-12 col-md-12 mb-12">
+	<div class="alert alert-primary" role="alert">
+		Welcome to the dashboard page, you are logged in as an <b><?= $name_of_employee ?></b>
+	</div>
+</div>
+
 <!-- Earnings (Monthly) Card Example -->
 <div class="col-xl-3 col-md-6 mb-4">
 	<div class="card border-left-primary shadow h-100 py-2">
@@ -5,11 +11,11 @@
 			<div class="row no-gutters align-items-center">
 				<div class="col mr-2">
 					<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-						Order</div>
-					<div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+						Item</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $item->item ?></div>
 				</div>
 				<div class="col-auto">
-					<i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+					<i class="fas fa-boxes fa-2x text-gray-300"></i>
 				</div>
 			</div>
 		</div>
@@ -23,11 +29,115 @@
 			<div class="row no-gutters align-items-center">
 				<div class="col mr-2">
 					<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-						Package</div>
-					<div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+						Material</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $material->material ?></div>
 				</div>
 				<div class="col-auto">
-					<i class="fas fa-boxes fa-2x text-gray-300"></i>
+					<i class="fas fa-cube fa-2x text-gray-300"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+	<div class="card border-left-warning shadow h-100 py-2">
+		<div class="card-body">
+			<div class="row no-gutters align-items-center">
+				<div class="col mr-2">
+					<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+						Request Submitted</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $req_sub->result ?></div>
+				</div>
+				<div class="col-auto">
+					<i class="fas fa-paper-plane fa-2x text-gray-300"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="col-xl-3 col-md-6 mb-4">
+	<div class="card border-left-primary shadow h-100 py-2">
+		<div class="card-body">
+			<div class="row no-gutters align-items-center">
+				<div class="col mr-2">
+					<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+						Request Accepted</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $req_acc->result ?></div>
+				</div>
+				<div class="col-auto">
+					<i class="fas fa-paper-plane fa-2x text-gray-300"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="col-xl-3 col-md-6 mb-4">
+	<div class="card border-left-danger shadow h-100 py-2">
+		<div class="card-body">
+			<div class="row no-gutters align-items-center">
+				<div class="col mr-2">
+					<div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+						Request Rejected</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $req_rej->result ?></div>
+				</div>
+				<div class="col-auto">
+					<i class="fas fa-paper-plane fa-2x text-gray-300"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+	<div class="card border-left-warning shadow h-100 py-2">
+		<div class="card-body">
+			<div class="row no-gutters align-items-center">
+				<div class="col mr-2">
+					<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+						Return</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $return->result ?></div>
+				</div>
+				<div class="col-auto">
+					<i class="fas fa-sync fa-2x text-gray-300"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+	<div class="card border-left-success shadow h-100 py-2">
+		<div class="card-body">
+			<div class="row no-gutters align-items-center">
+				<div class="col mr-2">
+					<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+						Roadmap</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $roadmap->result ?></div>
+				</div>
+				<div class="col-auto">
+					<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+	<div class="card border-left-primary shadow h-100 py-2">
+		<div class="card-body">
+			<div class="row no-gutters align-items-center">
+				<div class="col mr-2">
+					<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+						Employee</div>
+					<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $employee->result ?></div>
+				</div>
+				<div class="col-auto">
+					<i class="fas fa-users fa-2x text-gray-300"></i>
 				</div>
 			</div>
 		</div>
